8cc626bac1968c88c5e7b195ce22a87c1278801e
Michael Mulley <michael@michaelmulley.com>
2011-01-10 15:10:41 -0500

    Rewrite to use different classes for different field types

--- dbarray/__init__.py
+++ dbarray/__init__.py
@@ -1,58 +1,23 @@
 from django.core.exceptions import FieldError, ValidationError
 from django.db import models
-from django.utils.importlib import import_module
 
-class ArrayField(models.Field):
-    """Django field type for an array of values.
+def require_postgres(connection):
+    engine = connection.settings_dict['ENGINE']
+    if 'psycopg2' not in engine and 'postgis' not in engine:
+        raise FieldError("Array fields are currently implemented only for PostgreSQL/psycopg2")
+
+class ArrayFieldBase(object):
+    """Django field type for an array of values. Supported only on PostgreSQL.
     
-    Values can be of any standard database type. Supported only on PostgreSQL."""
+    This class is not meant to be instantiated directly; instead, field classes
+    should inherit from this class and from an appropriate Django model class.
+    """
     
-    __metaclass__ = models.SubfieldBase
-    
-    description = "An array of values (of any single database type)"
-    
-    def __init__(self, *args, **kwargs):
-        """Takes one required keyword argument, fieldtype, which is the class of the 
-        field type we want an array of.
-        
-        All other arguments will be passed to the field type.
-        
-        e.g. ArrayField(fieldtype=models.CharField, max_length=20)
-        
-        """
-        
-        self.fieldclass = kwargs.pop('fieldtype', None)
-        super(ArrayField, self).__init__()
-        
-        # For the fieldtype argument, as well as a class object,
-        # we accept a string of a full class path.
-        # e.g. "django.db.models.field.IntegerField"
-        # This is in order to play nicely with South
-        if isinstance(self.fieldclass, basestring):
-            components = self.fieldclass.split('.')
-            classname = components.pop()
-            module = import_module('.'.join(components))
-            self.fieldclass = getattr(module, classname)
-            
-        if not isinstance(self.fieldclass, type) \
-          and issubclass(self.fieldclass, models.Field):
-            raise FieldError("ArrayField must be given Field instance as a "
-                "keyword argument called fieldtype")
-                
-        self.fieldtype = self.fieldclass(*args, **kwargs)
-        self.validators = self.fieldtype.validators
-        self.null = self.fieldtype.null
-        self.default = kwargs.pop('default', None)
-    
-    def contribute_to_class(self, *args, **kwargs):
-        super(ArrayField, self).contribute_to_class(*args, **kwargs)
-        self.fieldtype.model = self.model
-        self.fieldtype.set_attributes_from_name(self.name)
+    _south_introspects = True
     
     def db_type(self, connection):
-        if 'post' not in connection.settings_dict['ENGINE']:
-            raise FieldError("ArrayField is currently implemented only for PostgreSQL")
-        return self.fieldtype.db_type(connection=connection) + '[]'
+        require_postgres(connection)
+        return super(ArrayFieldBase, self).db_type(connection=connection) + '[]'
         
     def to_python(self, value):
         # psycopg2 already supports array types, so we don't actually need to serialize
@@ -64,42 +29,40 @@ class ArrayField(models.Field):
                 iter(value)
             except TypeError:
                 raise ValidationError("An ArrayField value must be None or an iterable.")
-        return [self.fieldtype.to_python(x) for x in value]
-        
-    def get_db_prep_value(self, value, connection, prepared=False):
-        if 'post' not in connection.settings_dict['ENGINE']:
-            raise FieldError("ArrayField is currently implemented only for PostgreSQL")
-        if value is None:
-            return None
-        return [self.fieldtype.get_db_prep_value(value=v, connection=connection, prepared=prepared)
-            for v in value]
+        s = super(ArrayFieldBase, self)
+        return [s.to_python(x) for x in value]
             
     def get_prep_value(self, value):
         if value is None:
             return None
-        return [self.fieldtype.get_prep_value(v) for v in value]
-        
-    def pre_save(self, model_instance, add):
-        return self.fieldtype.pre_save(model_instance, add)
-        
-    def get_prep_lookup(self, lookup_type, value):
-        return self.fieldtype.get_prep_lookup(lookup_type, value)
-        
-    def get_db_prep_lookup(self, lookup_type, value, connection, prepared=False):
-        return self.fieldtype.get_db_prep_lookup(lookup_type, value, connection, prepared)
-                
-    def south_field_triple(self):
-        "Returns a suitable description of this field for South."
-        from south.modelsinspector import introspector
-        parent_field_class = self.fieldclass.__module__ + "." + self.fieldclass.__name__
-        our_field_class = self.__class__.__module__ + "." + self.__class__.__name__
-        args, kwargs = introspector(self.fieldtype)
-        kwargs['fieldtype'] = repr(parent_field_class)
-        return (our_field_class, args, kwargs)
+        s = super(ArrayFieldBase, self)
+        return [s.get_prep_value(v) for v in value]
         
     def run_validators(self, value):
-        if value is not None:
+        if value is None:
+            super(ArrayFieldBase, self).run_validators(value)
+        else:
             for v in value:
-                super(ArrayField, self).run_validators(v)
+                super(ArrayFieldBase, self).run_validators(v)
+                
+class ArrayFieldMetaclass(models.SubfieldBase):
+    pass
+
+def array_field_factory(name, fieldtype, module=ArrayFieldBase.__module__):
+    return ArrayFieldMetaclass(name, (ArrayFieldBase, fieldtype),
+        {'__module__': module,
+        'description': "An array, where each element is of the same type "\
+        "as %s." % fieldtype.__name__})
         
+# If you want to make an array version of a field not covered below, this is
+# the easiest way:
+# 
+# class FooArrayField(dbarray.ArrayFieldBase, FooField):
+#     __metaclass__ = dbarray.ArrayFieldMetaclass
+                
+IntegerArrayField = array_field_factory('IntegerArrayField', models.IntegerField)
+FloatArrayField = array_field_factory('FloatArrayField', models.FloatField)
+CharArrayField = array_field_factory('CharArrayField', models.CharField)
+TextArrayField = array_field_factory('TextArrayField', models.TextField)
+
     
